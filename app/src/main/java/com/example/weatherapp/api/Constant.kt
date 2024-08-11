cc6b25c6a02b2ece03dda2861be54e35ee791707
Dell <vswati0101@gmail.com>
2024-08-11 14:20:52 +0530

    Integrated api

---
+++ app/src/main/java/com/example/weatherapp/api/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp.api
+
+object Constant {
+    val apiKey="Enter Your Api key"
+}
---
+++ app/src/main/java/com/example/weatherapp/api/NetworkResponse.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.api
+// T refers to weatherModel
+sealed class NetworkResponse<out T> {
+    data class Success<out T>(val data:T):NetworkResponse<T>()
+    data class Error(val message:String):NetworkResponse<Nothing>()
+    object Loading:NetworkResponse<Nothing>()
+}
